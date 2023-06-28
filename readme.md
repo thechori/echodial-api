18d2dd774df17a0f986428f521b0effeb01bba5c
Ryan Teodoro <ryan.h.teodoro@gmail.com>
2023-06-28 14:12:46 -0500

    finally get programmatic voice working

---
+++ readme.md
@@ -0,0 +1,7 @@
+# L34ds
+
+This is a project
+
+## Notes
+
+- Kept getting errors about my numbers not belonging to me, but this was BS. It was simply due to the fact that I was using the Test SID and Test Auth Token...
--- scripts/contact-leads.js
+++ scripts/contact-leads.js
@@ -8,10 +8,29 @@ console.log("authToken: ", authToken);
 
 const client = require("twilio")(accountSid, authToken);
 
-client.messages
+// client.messages
+//   .create({
+//     body: "Hello from twilio-node",
+//     to: "+18326460869", // Text your number
+//     from: "+12812068992", // From a valid Twilio number
+//   })
+//   .then((message) => console.log(message.sid));
+
+client.calls
   .create({
-    body: "Hello from twilio-node",
-    to: "+18326460869", // Text your number
-    from: "+12812068992", // From a valid Twilio number
+    url: "http://demo.twilio.com/docs/voice.xml",
+    to: "+18326460869",
+    // from: "+12813178765",
+    from: "+12812068992",
   })
-  .then((message) => console.log(message.sid));
+  .then((call) => console.log(call.sid));
+
+// client.calls
+//   .create({
+//     method: "POST",
+//     sendDigits: "1234#",
+//     url: "https://demo.twilio.com/welcome/voice/",
+//     to: "+18326460869",
+//     from: "+12812068992",
+//   })
+//   .then((call) => console.log(call.sid));
