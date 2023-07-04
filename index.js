9bb33282577069ebd7ac5c2e2229759e0501aa1e
Ryan Teodoro <ryan.h.teodoro@gmail.com>
2023-07-04 15:50:50 -0500

    improve /lead/pretty endpoint

--- index.js
+++ index.js
@@ -37,8 +37,14 @@ app.get("/lead", async (req, res) => {
 app.get("/lead/pretty", async (req, res) => {
   const leads = await db("development.lead")
     .join("development.person", "person_id", "person.id")
-    .join("development.campaign", "campaign_id", "campaign.id");
-  // .select("development.lead.id", "development.lead.created_at", "ca");
+    .join("development.campaign", "campaign_id", "campaign.id")
+    .select(
+      "development.lead.id",
+      "development.lead.created_at",
+      "development.lead.body as message",
+      "development.person.phone as person_phone",
+      "development.campaign.name as campaign_name"
+    );
   res.status(200).send(leads);
 });
 
