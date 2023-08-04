86786364c48b9663cea46a97fbd4ae8879495ee6
Ryan Teodoro <ryan.h.teodoro@gmail.com>
2023-08-03 22:49:35 -0500

    attach verified jwt to res.locals for easy access

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -9,13 +9,23 @@ dotenv.config();
 export const authMiddleware: RequestHandler = async (req, res, next) => {
   const { authorization } = req.headers;
 
+  // Check for authorization field
   if (!authorization) {
     return res.status(401).json({ message: "Unauthorized request" });
   }
 
   try {
+    // Extract and verify JWT
     const token = authorization.split(" ")[1];
-    await jwt.verify(token, process.env.BCRYPT_SECRET as string);
+    const jwtVerified = await jwt.verify(
+      token,
+      process.env.BCRYPT_SECRET as string
+    );
+
+    // Attach JWT to req for each access
+    res.locals.jwt = jwtVerified;
+
+    // Continue
     return next();
   } catch (error) {
     return res.status(500).json({ message: extractErrorMessage(error) });
