00a5f19b238d80f6414b90872f54dfe0ce06d89d
Ryan Teodoro <ryan.h.teodoro@gmail.com>
2023-10-30 02:50:18 -0500

    expose lead standard properties via /GET endpoint

--- src/routes/lead-custom-property.ts
+++ src/routes/lead-custom-property.ts
@@ -8,11 +8,29 @@ const router = Router();
 
 // Return LeadCustomProperty items
 router.get("/", async (req, res) => {
+  const { id } = res.locals.jwt_decoded;
+
   try {
+    // Only return custom properties that have been created by the specific user
     const leadCustomProperties: LeadCustomProperty[] = await db(
       "lead_custom_property"
-    );
+    ).where("user_id", id);
     return res.status(200).send(leadCustomProperties);
+  } catch (e) {
+    return res.status(500).send({ message: extractErrorMessage(e) });
+  }
+});
+
+// Create new item
+// {
+//   label //
+//   value // API creates based on label passed - spaces as delimiters
+//   description?
+// }
+router.post("/", async (req, res) => {
+  const { id } = res.locals.jwt_decoded;
+
+  try {
   } catch (e) {
     return res.status(500).send({ message: extractErrorMessage(e) });
   }
