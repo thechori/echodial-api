83c75bb6489c61e7e422463a02e7637d1d72f688
Ryan Teodoro <ryan.h.teodoro@gmail.com>
2023-11-04 00:50:13 -0500

    finish basic functioning POST endpoint for creating LeadCustomProperty

--- src/routes/lead.property.custom.ts
+++ src/routes/lead.property.custom.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import db from "../utils/db";
 import { extractErrorMessage } from "../utils/error";
 import { LeadCustomProperty } from "../types";
+import { createValueFromLabel } from "../utils/helpers/create-value-from-label";
 
 const router = Router({ mergeParams: true });
 
@@ -29,8 +30,42 @@ router.get("/", async (req, res) => {
 // }
 router.post("/", async (req, res) => {
   const { id } = res.locals.jwt_decoded;
+  const {
+    lead_property_group_id,
+    lead_property_type_id,
+    name,
+    label,
+    description,
+  } = req.body;
+
+  /* Check for required fields */
+
+  if (lead_property_group_id === null) {
+    return res
+      .status(400)
+      .send({ message: "Missing `lead_property_group_id` field" });
+  }
+  if (lead_property_type_id === null) {
+    return res
+      .status(400)
+      .send({ message: "Missing `lead_property_type_id` field" });
+  }
+  if (label === null) {
+    return res.status(400).send({ message: "Missing `label` field" });
+  }
 
   try {
+    const newLeadCustomProperty = await db("lead_custom_property").insert({
+      user_id: id,
+      id,
+      lead_property_group_id,
+      lead_property_type_id,
+      label,
+      name: createValueFromLabel(label),
+      description,
+    });
+
+    return res.status(200).send(newLeadCustomProperty);
   } catch (e) {
     return res.status(500).send({ message: extractErrorMessage(e) });
   }
