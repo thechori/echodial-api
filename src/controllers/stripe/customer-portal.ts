a9969de2691959ab0e64de41ec9f5be75a3d41c0
Ryan Teodoro <ryan.h.teodoro@gmail.com>
2023-11-14 01:04:52 -0600

    remove unnecessary definition of configuration object (works without this, added unnecessary complexity and issues that would've arisen with ids that were environment specific - big no no)

--- src/controllers/stripe/customer-portal.ts
+++ src/controllers/stripe/customer-portal.ts
@@ -37,48 +37,9 @@ export const createStripeCustomerPortalSession = async (
     throw Error("Stripe customer id not found");
   }
 
-  const configuration = await stripe.billingPortal.configurations.create({
-    business_profile: {
-      headline: "EchoDial partners with Stripe for simplified billing.",
-    },
-    features: {
-      invoice_history: {
-        enabled: true,
-      },
-      customer_update: {
-        enabled: true,
-        allowed_updates: ["name"],
-      },
-      payment_method_update: {
-        enabled: true,
-      },
-      subscription_update: {
-        enabled: true,
-        default_allowed_updates: ["price"],
-        products: [
-          {
-            prices: ["price_1NvS4dKGxd0U3zJwIewutyC0"],
-            product: "prod_OitsykoSUqeKpi", // standard
-          },
-          {
-            prices: ["price_1NvS4yKGxd0U3zJwoeFX6FJL"],
-            product: "prod_OitsB8Q8HQzlt6", // pro
-          },
-          {
-            prices: ["price_1NvS5LKGxd0U3zJwR3hiVSZe"],
-            product: "prod_OittJ702oronDJ", // unlimited
-          },
-        ],
-      },
-    },
-  });
-
-  console.log("configuration", configuration);
-
   const session = await stripe.billingPortal.sessions.create({
     customer: customerId,
     return_url: `${envConfig.clientHost}/subscription/callback`,
-    configuration: configuration.id,
   });
 
   res.status(200).send(session);
